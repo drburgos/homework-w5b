96d805b733857cdbaffd8e86bc60ade969ec6cdf
Joaquin Burgos <joaquin.burgos@gmx.com>
2021-04-14 19:38:10 -0500

    Final Changes

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-// current day displayed in header using id=currentDay.
+// Actual Status of Code Offered: current day displayed in header using id=currentDay.
 
-// each time block is color coded to show if it is past(grey), present(red),future(green).
+// Main complication: Each time block should be color coded to show if it is past(grey), present(red),future(green).
 
-// example: current time is red(present), if curent time is 11am everything before 11am is grey(past). else everyhing after 11am is green(future). 
+// Functionality: When clicking on time block enter input from user (send to LocalStorage). Save input to local storage when save button is clicked (when refreshed info is not erased --> preventDefault).
 
-// when clicking on time block enter input from user.
-// save input to local storage when save button is clicked (when refreshed info is not erased).
-
-// on click function
+// On click function (Equiavalent to eventlistner in vanila js)
 $(".saveButton").on("click", function (event) {
-    // prevents refresh when saveButton is clicked
+    // Prevents refresh when saveButton is clicked
    event.preventDefault();
 
-    // variable "textarea" holds the previous sibling tag object of saveButton which is textarea.
+    // Variable "textarea" holds the previous sibling tag object of saveButton which is textarea.
     var textarea = $(this).prev();
 
-    // variable "id" holds (sets variable textarea to its id)
+    // Variable "id" sets variable textarea to its id
     // Method attribute when it only receives the key, it reads the id. 
     // Without setting an id, it would be impossible to know to which node the text will belong to. 
     var id = textarea.attr("id");
 console.log(id);
 
     // We need to save the written text in a variable for storage.
-    // variable "value" holds (the value of textarea)
-    // when you don't pass a value to .val() it reads. 
+    // Variable "value" holds (the value of textarea)
+    // When you don't pass a value to .val() it reads. 
     var value = textarea.val();
     
     // Once we have the key + the value (in variables)
-    // we proceed to sets items id and value in local storage
+    // We proceed to set item's id and value in local storage
     localStorage.setItem(id, value);
 });
 
 // I need to reed all the blocks to see if there is a string saved in local storage. 
-// get items from local storage and set them to variables
+// Using method getitems from local storage and put them inside variables
 var textareaVal9 = localStorage.getItem("hr-9");
 var textareaVal10 = localStorage.getItem("hr-10");
 var textareaVal11 = localStorage.getItem("hr-11");
@@ -44,12 +41,13 @@ var textareaVal4 = localStorage.getItem("hr-4");
 var textareaVal5 = localStorage.getItem("hr-5");
 console.log(textareaVal9);
 
-// selecting the jquery node object and assigning it to a variable
-// specifically assigning id's to to variables
+// I'm selecting the jquery node object and assigning it to a variable
+// Specifically assigning id's to to variables
+// For learning purposes I'm doing half (morning hours) in vanilla js and half in jquery (afternoon hours). Functionality will remain the same.
 var textarea9 = document.getElementById("hr-9");
-var textarea10 = $("#hr-10");
-var textarea11 = $("#hr-11");
-var textarea12 = $("#hr-12");
+var textarea10 = document.getElementById("hr-10");
+var textarea11 = document.getElementById("hr-11");
+var textarea12 = document.getElementById("hr-12");;
 var textarea1 = $("#hr-1");
 var textarea2 = $("#hr-2");
 var textarea3 = $("#hr-3");
@@ -57,11 +55,12 @@ var textarea4 = $("#hr-4");
 var textarea5 = $("#hr-5");
 console.log(textarea9);
 
-// the values from local storage are displayed in the textarea by using id variables
+// The values from local storage are displayed in the textarea by using id variables
+// For learning purposes I'm doing half (morning hours) in vanilla js and half in jquery (afternoon hours). Functionality will remain the same.
 textarea9.value=textareaVal9;
-textarea10.val(textareaVal10);
-textarea11.val(textareaVal11);
-textarea12.val(textareaVal12);
+textarea10.value=textareaVal10;
+textarea11.value=textareaVal11;
+textarea12.value=textareaVal12;
 textarea1.val(textareaVal1);
 textarea2.val(textareaVal2);
 textarea3.val(textareaVal3);
@@ -69,46 +68,46 @@ textarea4.val(textareaVal4);
 textarea5.val(textareaVal5);
 console.log(textarea9);
 
-// variable "date" holds current date.
+// Creating variable "date" which holds current date.
 var date = moment().format("dddd, MMMM Do, YYYY");
 
-// display date in currentDay id 
+// Displaying the date in currentDay id by changin the text
 $("#currentDay").text(date);
 
-// defining time and colorBlockZones function
+// Defining time and colorBlockZones function
 function colorBlockZones() {
 
     // I want to obtain the current hour so: I called moment (https://stackoverflow.com/questions/38981976/moment-get-current-date) and then extracted the hours and stored them in a variable called "currentHour" which now holds the current hour.
     var currentHour = moment().hours();
 
-    // function for each class block to ditermine if it's past, present, or future
+    // Function for each class block to determine if it's past, present, or future
     $(".block").each(function() {
 
-        // variable "blockHour" holds id hour from class block and parseInt is used to change it from a string to an integer.
+        // Variable "blockHour" holds id hour from class block and parseInt is used to change it from a string to an integer.
         var blockHour = parseInt($(this).attr("id"));
 
         // We need to compare the variable blockHour (integer inside) with currentHour.
-        // if statement to ditermin if in the past hour's
+        // Adding if statement to determine if in the past hour's
         if (blockHour < currentHour) {
-            // adds color grey to blocks from "past"
+            // Adds color grey to blocks from "past"
             $(this).addClass("past");
         }
 
-        //else if statement to ditermine if in the present hour
+        // The else if statement is to determine if it's the current hour
         else if (blockHour === currentHour) {
-            // removes color grey from blocks
+            // Removes color grey from blocks
             $(this).removeClass("past");
-            // and adds color red to blocks
+            // Adds color red to blocks
             $(this).addClass("present");
         }
 
-        // else statement to ditermine if it's in the future (rest of options --> else)
+        // Last else statement to determine if it's in the future (rest of options --> else)
         else {
-            // removes grey from blocks
+            // Removes grey from blocks
             $(this).removeClass("past");
-            // removes red from blocks
+            // Removes red from blocks
             $(this).removeClass("present");
-            // adds green to the blocks
+            // Adds green to the blocks
             $(this).addClass("future");
         }
 
@@ -116,7 +115,7 @@ function colorBlockZones() {
 
 };
 
-// call the funtion colorBlockZones() to be executed to have the color that corresponds to the time.
+// Calling the funtion colorBlockZones() to be executed in order to have the color that corresponds to the time.
 colorBlockZones();
 
 
